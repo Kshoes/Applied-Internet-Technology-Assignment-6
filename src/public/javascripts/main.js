12925a2b88b2fd076556d7548ceb1fad930fd6de
Kshoes <kx360@nyu.edu>
2019-11-14 07:54:23 -0500

    finish requirements, but still buggy getCardNumValue function or computeTotal function

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -12,7 +12,7 @@ function main() {
 
         const startValueStrArray = startValueString.split(',');
         startValueStrArray.map(function(i) {
-            const card = new Card(i, getRandomSuit());
+            const card = new Card(i.toString(), getRandomSuit());
             startValues.push(card);  // reverse order so that it will be inserted in correct order in deck
         });
 
@@ -21,10 +21,10 @@ function main() {
         const computerHand = [];
         const playerHand = [];
 
-        computerHand.unshift(deck.deck.shift()); // dealing two first cards to each player
-        playerHand.unshift(deck.deck.shift());
-        computerHand.unshift(deck.deck.shift());
-        playerHand.unshift(deck.deck.shift());
+        computerHand.push(deck.deck.shift()); // dealing two first cards to each player
+        playerHand.push(deck.deck.shift());
+        computerHand.push(deck.deck.shift());
+        playerHand.push(deck.deck.shift());
 
         //
 
@@ -40,8 +40,12 @@ function main() {
         const playerSideDisplay = document.createElement('div');
         game.appendChild(playerSideDisplay);
 
-        computerSideDisplay.appendChild(document.createTextNode("Computer Hand - Total: ?"));  // computer total count
-        playerSideDisplay.appendChild(document.createTextNode("Player Hand - Total : " + playerTotal)); // player total count
+        computerTotalDisplay = document.createElement('h4');
+        computerTotalDisplay.textContent = "Computer Hand - Total: ?";
+        computerSideDisplay.appendChild(computerTotalDisplay);  // computer total count
+        playerTotalDisplay = document.createElement('h4');
+        playerTotalDisplay.textContent = "Player Hand - Total: " + playerTotal;
+        playerSideDisplay.appendChild(playerTotalDisplay);    // player total count
 
         for(let i = 0; i < computerHand.length; i++) {
             const cardDisplay = document.createElement('div');
@@ -69,6 +73,70 @@ function main() {
         standButton.textContent = "Stand";
         game.appendChild(standButton);
 
+        hitButton.addEventListener('click', function(evt) {
+
+            playerHand.push(deck.deck.shift());
+
+            const cardDisplay = document.createElement('div');
+            cardDisplay.textContent += playerHand[playerHand.length-1].value + "\n";
+            cardDisplay.textContent += playerHand[playerHand.length-1].suit;
+            cardDisplay.setAttribute('class', "card");
+            playerSideDisplay.appendChild(cardDisplay);
+
+            playerTotal = computeTotal(playerHand);
+            playerTotalDisplay.textContent = "Computer Hand - Total: " + playerTotal;
+            if(playerTotal > 21) {
+                playerSideDisplay.appendChild(document.createTextNode("Player Lost (Bust!)"));
+                endGame();
+            }
+            else if(computeTotal(playerHand) === 21) {
+                playerSideDisplay.appendChild(document.createTextNode("Blackjack! Press stand!"));
+            }
+            else {
+
+            }
+        });
+
+        standButton.addEventListener('click', function(evt) {
+
+            while(computerTotal < 17) {
+
+                computerHand.push(deck.deck.shift());
+
+                const cardDisplay = document.createElement('div');
+                cardDisplay.textContent += computerHand[computerHand.length].value + "\n";
+                cardDisplay.textContent += computerHand[computerHand.length].suit;
+                cardDisplay.setAttribute('class', "card");
+                computerSideDisplay.appendChild(cardDisplay);
+
+                computerTotal = computeTotal(computerHand);
+                computerTotalDisplay.textContent = "Computer Hand - Total: " + computerTotal;
+
+            }
+
+            // Determine winner
+
+            if(computerTotal > 21) {
+                computerSideDisplay.appendChild(document.createTextNode("Computer Lost (Bust!)"));
+            }
+            else if(playerTotal > computerTotal) {
+                playerSideDisplay.appendChild(document.createTextNode("Player Won! Final Score:     Player - " + playerTotal + " Computer - " + computerTotal));
+            }
+            else if(playerTotal < computerTotal) {
+                computerSideDisplay.appendChild(document.createTextNode("Computer Won! Final Score:     Player - " + playerTotal + " Computer - " + computerTotal));
+            }
+            else {
+                playerSideDisplay.appendChild(document.createTextNode("Tie! Final Score:     Player - " + playerTotal + " Computer - " + computerTotal));
+            }
+            endGame();
+        });
+
+        function endGame() {
+            hitButton.style.display = "none";
+            standButton.style.display = "none";
+            console.log('Game over!');
+        }
+
     });
 }
 document.addEventListener('DOMContentLoaded', main);
@@ -77,19 +145,19 @@ document.addEventListener('DOMContentLoaded', main);
 function getCardNumValue(val) {
     let numVal = 0;
     switch(val) {
-        case "2": numval = 2; break;
-        case "3": numval = 3; break;
-        case "4": numval = 4; break;
-        case "5": numval = 5; break;
-        case "6": numval = 6; break;
-        case "7": numval = 7; break;
-        case "8": numval = 8; break;
-        case "9": numval = 9; break;
-        case "10":
-        case "J":
-        case "Q":
-        case "K": numVal = 10; break;
-        case "A": numVal = 11; break;
+        case '2': numval = 2; break;
+        case '3': numval = 3; break;
+        case '4': numval = 4; break;
+        case '5': numval = 5; break;
+        case '6': numval = 6; break;
+        case '7': numval = 7; break;
+        case '8': numval = 8; break;
+        case '9': numval = 9; break;
+        case '10':
+        case 'J':
+        case 'Q':
+        case 'K': numVal = 10; break;
+        case 'A': numVal = 11; break;
     }
     return numVal;
 }
@@ -121,16 +189,13 @@ function computeTotal(hand) {
 function getRandomSuit() {
     const suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
     const randomSuit = suits[Math.floor(Math.random()*suits.length)];
-    console.log(randomSuit);
     return randomSuit;
 }
 
-// function add
-
 class Card {
     constructor(value, suit) {
-        this.value = value;
-        this.suit = suit;
+        this.value = value + '';
+        this.suit = suit + '';
     }
 }
 
@@ -153,12 +218,12 @@ class Deck {
         this.shuffle();
         startValues.map((i) => {    // start from last value of startValues
             const card = this.deck.find(deckI => (deckI.value === i.value && deckI.suit === i.suit));   // find card in shuffled deck
-            console.log(JSON.stringify(card));
+            // console.log(JSON.stringify(card));
             const cardIndex = this.deck.findIndex(deckI => (deckI.value === i.value && deckI.suit === i.suit));
-            console.log(cardIndex);
+            // console.log(cardIndex);
             this.deck.splice(cardIndex, 1)
             this.deck.unshift(card);  // move to top of deck
-            console.log(this.deck[0]);
+            // console.log(this.deck[0]);
         });
 
     }
