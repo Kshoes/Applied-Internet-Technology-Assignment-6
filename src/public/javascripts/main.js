2796fd733a9560773648f7b47e26b27b7c3b3b73
Kshoes <kx360@nyu.edu>
2019-11-13 19:24:07 -0500

    add handle form submission functionality

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,1 +1,13 @@
 // main.js
+
+function main(){
+
+    const play = document.querySelector('.playBtn');
+    play.addEventListener('click', function(evt) {
+        document.querySelector('form').style.display = "none";
+        evt.preventDefault();
+    })
+
+}
+document.addEventListener('DOMContentLoaded', main);
+
